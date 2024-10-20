d2026de1a456fb1ec6f637595eb2d764b295fcd9
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-20 11:29:07 -0500

    chore: initial commit

---
+++ src/main/java/com/acme/vortex/platform/VortexPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.vortex.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VortexPlatformApplication.class, args);
+    }
+
+}
