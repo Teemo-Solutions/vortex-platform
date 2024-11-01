d78d1eebfa26d7c42578d46af33b7444810c9300
vquijandria <u201822697@upc.edu.pe>
2024-11-01 13:58:43 -0500

    feat(User): added the UserQueryService

--- src/main/java/com/acme/vortex/platform/user/application/internal/queryservices/UserQueryServiceImpl.java
+++ src/main/java/com/acme/vortex/platform/user/application/internal/queryservices/UserQueryServiceImpl.java
@@ -1,9 +1,9 @@
 package com.acme.vortex.platform.user.application.internal.queryservices;
 
 import com.acme.vortex.platform.user.domain.model.aggregates.User;
-import com.acme.vortex.platform.user.domain.model.queries.GetAllUsersQuery;
 import com.acme.vortex.platform.user.domain.model.queries.GetUserByIdQuery;
 import com.acme.vortex.platform.user.domain.model.queries.GetUserByEmailQuery;
+import com.acme.vortex.platform.user.domain.model.queries.GetAllUsersQuery;
 import com.acme.vortex.platform.user.domain.services.UserQueryService;
 import com.acme.vortex.platform.user.infrastructure.persistence.jpa.UserRepository;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,31 @@ public class UserQueryServiceImpl implements UserQueryService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * @param query
+     * @return
+     */
     @Override
-    public List
+    public List<User> handle(GetAllUsersQuery query) {
+        return userRepository.findAllUsers();
+    }
+
+    /**
+     * @param query
+     * @return
+     */
+    @Override
+    public Optional<User> handle(GetUserByIdQuery query) {
+        return userRepository.findById(query.id());
+    }
+
+    /**
+     * @param query
+     * @return
+     */
+    @Override
+    public Optional<User> handle(GetUserByEmailQuery query) {
+        return userRepository.findByEmail(query.email());
+    }
 }
+
--- src/main/java/com/acme/vortex/platform/user/domain/services/UserQueryService.java
+++ src/main/java/com/acme/vortex/platform/user/domain/services/UserQueryService.java
@@ -1,8 +1,13 @@
 package com.acme.vortex.platform.user.domain.services;
 
 import com.acme.vortex.platform.user.domain.model.aggregates.User;
-import com.acme.vortex.platform.user.domain.model.queries.GetUserByEmailQuery;
+import com.acme.vortex.platform.user.domain.model.queries.GetAllUsersQuery;
 import com.acme.vortex.platform.user.domain.model.queries.GetUserByIdQuery;
+import com.acme.vortex.platform.user.domain.model.queries.GetUserByEmailQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserQueryService {
     List<User> handle(GetAllUsersQuery query);
