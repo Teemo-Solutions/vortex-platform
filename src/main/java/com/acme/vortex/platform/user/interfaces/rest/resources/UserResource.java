22e9df7ea609741a5f7cd7f0ab64102074859cf3
Vicente Quijandria <vquijandria@hotmail.com>
2024-11-01 12:22:24 -0500

    feat(UserResource): created the UserResource

---
+++ src/main/java/com/acme/vortex/platform/user/interfaces/rest/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.acme.vortex.platform.user.interfaces.rest.resources;
+
+public record UserResource(Long id, String name, String email, String role, String birthDate) {
+    public UserResource {
+        if (id == null || id < 0)
+            throw new IllegalArgumentException("id cannot be null");
+
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("name cannot be null or empty");
+
+        if (email == null || email.isBlank())
+            throw new IllegalArgumentException("email cannot be null or empty");
+
+        if (role == null || role.isBlank())
+            throw new IllegalArgumentException("role cannot be null or empty");
+
+        if (birthDate == null)
+            throw new IllegalArgumentException("birthDate cannot be null");
+
+    }
+}
