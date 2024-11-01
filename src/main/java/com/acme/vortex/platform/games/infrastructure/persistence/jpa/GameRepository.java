fa7ac42cac66ff98f58b108017ed251c2c3a4089
JoseRiega <u202211254@upc.edu.pe>
2024-11-01 12:12:02 -0500

    feat: Create GameRepository

--- src/main/java/com/acme/vortex/platform/games/infrastructure/persistence/jpa/GameRepository.java
+++ src/main/java/com/acme/vortex/platform/games/infrastructure/persistence/jpa/GameRepository.java
@@ -4,6 +4,11 @@ import com.acme.vortex.platform.games.domain.model.aggregates.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByTitleContainingIgnoreCase(String title);
+    Optional<Game> findByTitleAndDeveloper(String title, String developer);
 }
