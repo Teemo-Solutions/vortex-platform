2b7161fd00fdc1d51c973dd58e27ff2837775e6d
vquijandria <u201822697@upc.edu.pe>
2024-11-19 02:02:30 -0500

    feat(IAM): implemented base structure, command and queries services for roles

---
+++ src/main/java/com/acme/vortex/platform/iam/domain/model/commands/SignUpCommand.java
@@ -0,0 +1,10 @@
+package com.acme.vortex.platform.iam.domain.model.commands;
+
+import com.acme.vortex.platform.iam.domain.model.entities.Role;
+
+import java.util.List;
+
+public record SignUpCommand(String username,
+                            String password,
+                            List<Role> roles) {
+}
---
+++ src/main/java/com/acme/vortex/platform/iam/interfaces/rest/resources/SignInResource.java
@@ -0,0 +1,4 @@
+package com.acme.vortex.platform.iam.interfaces.rest.resources;
+
+public record SignInResource(String username, String password) {
+}
